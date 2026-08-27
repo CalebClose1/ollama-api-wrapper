264dcc59510b343ea83e49f4522e8365a87a3b45
Caleb Close <CalebClose1@gmail.com>
2026-08-27 11:08:46 -0400

    Add continuous chat loop

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ import requests     #importing the requests Library
 
 url = "http://localhost:11434/api/generate" #Local Host on my computer with the API listening on port 11434
 
-user_input = input("Question: ")    #Take user input for question 
+while True:     #While loop to allow for continuous questions
+    user_input = input("Question: ")    #Take user input for question 
 
-data = {        #Python Dictionary
-    "model": "llama3.2",
-    "prompt": user_input,
-    "stream": True
-}
+    if user_input.lower() == 'exit':
+        break
+
+    data = {        #Python Dictionary
+        "model": "llama3.2",
+        "prompt": user_input,
+        "stream": False
+    }
 
 
-response = requests.post(url, json = data)  #Send an HTTP POST request to url and include the contents of data as JSON.
+    response = requests.post(url, json = data)  #Send an HTTP POST request to url and include the contents of data as JSON.
 
-result = response.json()
-print(result["response"])
+    result = response.json()
+    print(result["response"])
